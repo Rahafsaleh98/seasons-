f0546f94494c34df6015d941d725297309bd726b
Rahaf Saleh <rahafalsaleh98@yahoo.com>
2021-01-27 15:45:48 +0200

    add new file

---
+++ app.js
@@ -0,0 +1,11 @@
+var seasons = prompt ('what is your favorite season ?');
+
+
+if (seasons == "Autumn") {
+    var seasonsName = prompt('what is name of season?')
+    alert("welcome")
+} else {
+    alert("Amazing !!") 
+}
+document.write('<b>' . Amman , '</b>')
+
